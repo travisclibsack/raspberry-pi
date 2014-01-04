68b55cedcf453cd93094d546faab3ce28328d87c
Travis Libsack <libsackt@hs.rsu5.org>
2014-01-03 22:46:05 -0500

    Update wipi.sh

--- PiFi/wipi.sh
+++ PiFi/wipi.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 #This configures your Raspberry Pi as a wireless router
 #Modified from Adafruit's tutorial (http://learn.adafruit.com/setting-up-a-raspberry-pi-as-a-wifi-access-point/overview)
+
+echo "(tput For making the Raspberry Pi into a wireless acess point.
+bases off of the adafruit tutorial 
 
