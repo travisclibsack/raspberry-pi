4d2e9957676efa3746be9e2379041531a66fa2e2
Travis Libsack <libsackt@hs.rsu5.org>
2014-02-21 22:35:53 -0500

    Update and rename camera_install to camera_install.sh

--- camera/camera_install.sh
+++ camera/camera_install.sh
@@ -2,6 +2,7 @@
 #remember to initialize the raspi camera option in raspiconfig!
 #!/bin/bash
 
+
 echo "Installing gst-streamer1.0 and mjpg-streamer"
 echo "need to also install dependencies"
 sudo apt-get install gst-streamer1.0
