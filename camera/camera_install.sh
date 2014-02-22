cb1bb53279651e66d35bc0699c92131dd4eca951
Travis Libsack <libsackt@hs.rsu5.org>
2014-02-21 22:58:56 -0500

    Update camera_install.sh

--- camera/camera_install.sh
+++ camera/camera_install.sh
@@ -2,6 +2,10 @@
 #remember to initialize the raspi camera option in raspiconfig!
 #!/bin/bash
 
+if (( $EUID != 0 )); then 
+   echo "$(tput setaf 1)This must be run as root. Try 'sudo bash $0'.$(tput sgr0)" 
+   exit 1 
+fi
 
 echo "Installing gst-streamer1.0 and mjpg-streamer"
 echo "need to also install dependencies"
